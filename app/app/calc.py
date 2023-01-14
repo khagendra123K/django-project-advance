4af11095ed10fb2d22008c2b07a8131cc8d5681c
user_name <email_id>
2023-01-14 15:34:58 +0545

    Configured docker compose and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+
+"""
+Calculator fuctions
+"""
+
+def add(x,y):
+    """Add x and y and return result."""
+    return x+y
+
+def sub(x,y):
+    """Add x and y and return result."""
+    return x-y
